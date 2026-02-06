3ea0a17f9f3104eda31f3a6feb7bf3d2d56be9a6
Handsome Jack <kirilldk11beloysov@gmail.com>
2026-02-06 20:31:08 +0300

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,174 @@
+
+local repo = 'https://raw.githubusercontent.com/hellotheren/hihitler/refs/heads/main/'
+local k = "null"
+local UIS = game:GetService("UserInputService")
+local HttpService = game:GetService("HttpService")
+if UIS.KeyboardEnabled then
+local Library = loadstring(game:HttpGet(repo .. 'library_main.lua'))()
+else
+local Library = loadstring(game:HttpGet(repo .. 'library_other.lua'))()
+end
+local ThemeManager = loadstring(game:HttpGet(repo .. 'library_theme.lua'))()
+local SaveManager = loadstring(game:HttpGet(repo .. 'library_save.lua'))()
+local Options = Library.Options
+local Toggles = Library.Toggles
+local requestFunc = (syn and syn.request) or (fluxus and fluxus.request) or (http and http.request) or http_request or request
+local TweenService = game:GetService("TweenService")
+local UserInputService = game:GetService("UserInputService")
+local CoreGui = game:GetService("CoreGui")
+local HttpService = game:GetService("HttpService")
+Library.ShowToggleFrameInKeybinds = true -- Make toggle keybinds work inside the keybinds UI (aka adds a toggle to the UI). Good for mobile users (Default value = true)
+Library.ShowCustomCursor = false -- Toggles the Linoria cursor globaly (Default value = true)
+Library.NotifySide = "Left" -- Changes the side of the notifications globaly (Left, Right) (Default value = Left)
+
+local Junkie = loadstring(game:HttpGet("https://jnkie.com/sdk/library.lua"))()
+Junkie.service = "YOUR_SERVICE"
+Junkie.identifier = "12345" 
+Junkie.provider = "Mixed"
+
+-- UI Implementation
+local validatedKey = nil
+
+-- Show custom UI 
+local function showUI()
+
+    local userKey = k  -- Replace with your UI input
+    
+    if userKey then
+        local validation = Junkie.check_key(userKey)
+        if validation.valid then
+			print("valid key")
+			return userKey
+        else
+            warn("Error: " .. (validation.error or "Invalid key"))
+            return nil
+        end
+    end
+end
+
+validatedKey = showUI()
+
+local Window = Library:CreateWindow({
+
+	Title = 'Key System',
+	Center = true,
+	AutoShow = true,
+	Resizable = true,
+	ShowCustomCursor = true, 
+	UnlockMouseWhileOpen = true,
+	NotifySide = "Left",
+	TabPadding = 8,
+	MenuFadeTime = 1
+})
+
+local Tabs = {
+
+	Main = Window:AddTab('Key'),
+}
+local TabBoxes = {
+    TabBox1 = Tabs.Main:AddLeftTabbox()
+}
+local TabBoxestabs = {
+    Tab1 = TabBoxes.TabBox1:AddTab('credits'),
+    Tab2 = TabBoxes.TabBox1:AddTab('key system')
+}
+local Discord = TabBoxestabs.Tab1:AddButton({
+	Text = 'Discord',
+	Func = function()
+		setclipboard("https://discord.gg/mtbjM9f3BD")
+		Library:Notify("Copied in clipboard!")
+        s,e = pcall(function()
+	requestFunc({
+	Url = 'http://127.0.0.1:6463/rpc?v=1',
+	Method = 'POST',
+	Headers = {
+		['Content-Type'] = 'application/json',
+		Origin = 'https://discord.com'
+	},
+	Body = HttpService:JSONEncode({
+		cmd = 'INVITE_BROWSER',
+		nonce = HttpService:GenerateGUID(false),
+		args = {code = "mtbjM9f3BD"}
+		})
+	})
+end)
+	end,
+	DoubleClick = false,
+
+	Tooltip = 'Discord',
+	DisabledTooltip = 'I am disabled!', -- Information shown when you hover over the button while it's disabled
+
+	Disabled = false, -- Will disable the button (true / false)
+	Visible = true -- Will make the button invisible (true / false)
+})
+local Unload = TabBoxestabs.Tab1:AddButton({
+	Text = 'Unload',
+	Func = function()
+		Library.Unload()
+	end,
+	DoubleClick = false,
+
+	Tooltip = 'will unload keysystem',
+	DisabledTooltip = 'I am disabled!', -- Information shown when you hover over the button while it's disabled
+
+	Disabled = false, -- Will disable the button (true / false)
+	Visible = true -- Will make the button invisible (true / false)
+})
+
+local GetKey = TabBoxestabs.Tab2:AddButton({
+	Text = 'Get Key',
+	Func = function()
+        local link = Junkie.get_key_link()
+		if link then
+			setclipboard(link)  -- Copy to clipboard
+			print("Link copied to clipboard!")
+		else
+			warn("Wait 5 minutes")
+			return nil
+		end
+	
+	end,
+	DoubleClick = false,
+
+	Tooltip = 'will copy key',
+	DisabledTooltip = 'I am disabled!', -- Information shown when you hover over the button while it's disabled
+
+	Disabled = false, -- Will disable the button (true / false)
+	Visible = true -- Will make the button invisible (true / false)
+})
+local InputBox = TabBoxestabs.Tab2:AddInput('InputBox', {
+	Default = 'Input Box',
+	Numeric = false, -- true / false, only allows numbers
+	Finished = false, -- true / false, only calls callback when you press enter
+	ClearTextOnFocus = true, -- true / false, if false the text will not clear when textbox focused
+		
+	Text = 'enter here key',
+	Tooltip = 'pls faster daddy', -- Information shown when you hover over the textbox
+
+	Placeholder = 'Skidded', -- placeholder text when the box is empty
+	-- MaxLength is also an option which is the max length of the text
+
+	Callback = function(Value)
+		k = Value
+	end,
+})
+local CheckKey = TabBoxestabs.Tab2:AddButton({
+	Text = 'Check Key',
+	Func = function()
+    
+	if not validatedKey then
+    	warn("No valid key provided")
+    	return
+	end
+
+
+	getgenv().SCRIPT_KEY = validatedKey
+	end,
+	DoubleClick = false,
+
+	Tooltip = 'will check key',
+	DisabledTooltip = 'I am disabled!', -- Information shown when you hover over the button while it's disabled
+
+	Disabled = false, -- Will disable the button (true / false)
+	Visible = true, -- Will make the button invisible (true / false)
+})
